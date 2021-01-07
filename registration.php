d30d2e5a9a48de6ddd64f507452dcc942dd55646
Yogesh Khasturi <34720956+yogeshkhasturi@users.noreply.github.com>
2021-01-07 13:22:52 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category   Webiators
+ * @package    Webiators_GeoIpRedirection
+ * @author     Webiators Team
+ * @copyright  Copyright (c) Webiators Technologies Private Limited. ( https://webiators.com ).
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Webiators_GeoIpRedirection',
+    __DIR__
+);
